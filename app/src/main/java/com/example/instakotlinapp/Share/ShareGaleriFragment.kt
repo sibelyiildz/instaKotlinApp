971bbcd8a79c6b70a9f67d8fa3a2184ad97720f7
Sibel Yıldız <sibelyldz2012@gmail.com>
2020-05-07 21:52:15 +0300

    ShareGaleri Fragmentinin tasarımı yapıldı.

--- app/src/main/java/com/example/instakotlinapp/Share/ShareGaleriFragment.kt
+++ app/src/main/java/com/example/instakotlinapp/Share/ShareGaleriFragment.kt
@@ -1,11 +1,14 @@
 package com.example.instakotlinapp.Share
 
 import android.os.Bundle
+import android.os.Environment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
 import com.example.instakotlinapp.R
+import kotlinx.android.synthetic.main.fragment_share_galeri.view.*
 
 /**
  * A simple [Fragment] subclass.
@@ -13,11 +16,40 @@ import com.example.instakotlinapp.R
 class ShareGaleriFragment : Fragment() {
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_share_galeri, container, false)
+        var view = inflater.inflate(R.layout.fragment_share_galeri, container, false)
+
+        var klasörlerYollari = ArrayList<String>()
+        var klasörAdlari = ArrayList<String>()//Kullanıcıya göstereceğimiz isimler
+
+
+        //Resimleri göstereceğim klasörlerin yolları
+        var root = Environment.getExternalStorageDirectory().path
+        var kameraResimleri = root + "/DCIM/Camera"
+        var indirilenResimler = root + "/Download"
+        var whatsappResimleri = root + "/Whatsapp/Media/Whatsapp Images"
+
+        klasörlerYollari.add(kameraResimleri)
+        klasörlerYollari.add(indirilenResimler)
+        klasörlerYollari.add(whatsappResimleri)
+
+        //Spinnerde gösterilecek adlar
+        klasörAdlari.add("Kamera")
+        klasörAdlari.add("İndirilenler")
+        klasörAdlari.add("Whatsapp")
+
+        //İsimleri spinnera adapter ile atamak
+        var spinnerArrayAdapter =
+            activity?.let { ArrayAdapter(it, R.layout.spinner_item, klasörAdlari) }
+        spinnerArrayAdapter!!.setDropDownViewResource(R.layout.spinner_dropdown_item)
+
+        //Oluşturduğumuz adapteri spinnere atıyoruz.
+        view.spnKlasörAdlari.adapter = spinnerArrayAdapter
+
+        return view
     }
 
 }
